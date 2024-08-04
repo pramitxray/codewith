c7c48a15067ecc4343a71bebdc659031dc84ab37
pramitxray <pramitranjan.ray2021@vitstudent.ac.in>
2024-08-04 20:00:24 +0530

    uploaded from local

--- client/src/Actions.js
+++ client/src/Actions.js
@@ -1,10 +1,10 @@
-const ACTIONS = {
-    JOIN: 'join',
-    JOINED: 'joined',
-    DISCONNECTED: 'disconnected',
-    CODE_CHANGE: 'code-change',
-    SYNC_CODE: 'sync-code',
-    LEAVE: 'leave',
-};
+// All the events
 
-module.exports = ACTIONS;
+export const ACTIONS = {
+  JOIN: "join",
+  JOINED: "joined",
+  DISCONNECTED: "disconnected",
+  CODE_CHANGE: "conde-change",
+  SYNC_CODE: "sync-code",
+  LEAVE: "leave",
+};
--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,21 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
+import Home from './components/Home';
+import EditorPage from './components/EditorPage';
 import { Toaster } from 'react-hot-toast';
-import Home from './pages/Home';
-import EditorPage from './pages/EditorPage';
 
 function App() {
-    return (
-        <>
-            <div>
-                <Toaster
-                    position="top-right"
-                    toastOptions={{
-                        success: {
-                            theme: {
-                                primary: '#4aed88',
-                            },
-                        },
-                    }}
-                ></Toaster>
-            </div>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home />}></Route>
-                    <Route
-                        path="/editor/:roomId"
-                        element={<EditorPage />}
-                    ></Route>
-                </Routes>
-            </BrowserRouter>
-        </>
-    );
+  return (
+    <>
+    <div>
+      <Toaster  position='top-center'></Toaster>
+    </div>
+    <Routes>
+     <Route path='/' element={ <Home /> } />
+     <Route path='/editor/:roomId' element={ <EditorPage /> } />
+    </Routes>
+    </>
+  );
 }
 
 export default App;
